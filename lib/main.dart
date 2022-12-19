f22c01d5b9851916d7bfb679a13804dec668631a
Nathan Pham <nathan.pham@legato.co>
2022-12-19 18:13:50 +0700

    feat: add auth mockup repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_base/src/core/domain/interfaces/lang_repository_interface.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'src/common/extensions/locale_x.dart';
@@ -10,8 +9,8 @@ import 'src/common/utils/global_bloc_observer.dart';
 import 'src/common/utils/logger.dart';
 import 'src/core/application/cubits/auth/auth_cubit.dart';
 import 'src/core/application/cubits/lang/lang_cubit.dart';
+import 'src/core/domain/interfaces/lang_repository_interface.dart';
 import 'src/core/infrastructure/datasources/local/storage.dart';
-import 'src/core/infrastructure/repositories/lang_repository.dart';
 import 'src/modules/app/app_router.dart';
 import 'src/modules/app/app_widget.dart';
 
